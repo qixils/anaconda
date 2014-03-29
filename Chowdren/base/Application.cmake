1b91e0488ee4cb38899bee30e984185166b77497
Mathias Kaerlev <matpow2@gmail.com>
2014-03-29 21:05:48 +0100

    Committing some missing files from the last commit

---
+++ Chowdren/base/Application.cmake
@@ -0,0 +1,9 @@
+cmake_minimum_required (VERSION 2.6)
+project(Chowdren)
+
+set(CHOWDREN_BASE_DIR "%(base_path)s")
+set(APP_NAME "%(name)s")
+set(APP_VERSION "%(version)s")
+set(APP_COPYRIGHT "%(copyright)s")
+
+include(${CHOWDREN_BASE_DIR}/CMakeLists.txt)

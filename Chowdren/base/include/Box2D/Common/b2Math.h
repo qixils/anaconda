80cbe5e248572c2339ecc1027babeaa00613ffb6
Mathias Kaerlev <matpow2@gmail.com>
2014-04-30 20:45:29 +0200

    Make sure we use std::isfinite and not the macro on GCC/Clang

--- Chowdren/base/include/Box2D/Common/b2Math.h
+++ Chowdren/base/include/Box2D/Common/b2Math.h
@@ -64,6 +64,8 @@ inline bool b2IsValid(float32 x)
 #elif __ghs__
 	return isfinite(x) != 0;
 #else
+	// make sure we aren't using the isfinite macro
+	#undef isfinite
 	return std::isfinite(x) != 0;
 #endif
 }

d83e006d49147bb3dde8a80e98304f193ef0dec8
Mathias Kaerlev <matpow2@gmail.com>
2014-05-08 21:42:50 +0200

    Oops, forgot grid.cpp and grid.h

---
+++ Chowdren/base/broadphase/grid.cpp
@@ -0,0 +1,113 @@
+#include "broadphase/grid.h"
+#include "manager.h"
+#include "frame.h"
+
+inline int div_ceil(int x, int y)
+{
+    return (x + y - 1) / y;
+}
+
+UniformGrid::UniformGrid()
+: query_id(0)
+{
+    width = div_ceil(global_manager->frame->width, GRID_SIZE);
+    height = div_ceil(global_manager->frame->height, GRID_SIZE);
+    grid = new GridItemList[width*height];
+}
+
+UniformGrid::~UniformGrid()
+{
+    delete[] grid;
+}
+
+void UniformGrid::clear()
+{
+    for (int i = 0; i < width*height; i++) {
+        grid[i].clear();
+    }
+}
+
+int UniformGrid::add(void * data, int v[4])
+{
+    int index;
+    if (free_list.empty()) {
+        index = store.size();
+        store.resize(index + 1);
+    } else {
+        index = free_list.back();
+        free_list.pop_back();
+    }
+
+    GridItem & item = store[index];
+    item.last_query_id = query_id;
+    item.data = data;
+    get_pos(v, item.box);
+
+    for (int x = item.box[0]; x < item.box[2]; x++)
+    for (int y = item.box[1]; y < item.box[3]; y++) {
+        grid[GRID_INDEX(x, y)].push_back(index);
+    }
+
+    return index;
+}
+
+inline void remove_proxy(GridItemList & list, int proxy)
+{
+    GridItemList::reverse_iterator it;
+    for (it = list.rbegin(); it != list.rend(); it++) {
+        if (*it != proxy)
+            continue;
+        list.erase(--(it.base()));
+        break;
+    }
+}
+
+void UniformGrid::remove(int proxy)
+{
+    GridItem & item = store[proxy];
+
+    for (int x = item.box[0]; x < item.box[2]; x++)
+    for (int y = item.box[1]; y < item.box[3]; y++) {
+        remove_proxy(grid[GRID_INDEX(x, y)], proxy);
+    }
+
+    item.data = NULL;
+    free_list.push_back(proxy);
+}
+
+inline bool overlaps(int x, int y, int box[4])
+{
+    return x >= box[0] && x < box[2] && y >= box[1] && y < box[3];
+}
+
+void UniformGrid::move(int proxy, int v[4])
+{
+    GridItem & item = store[proxy];
+    int box[4];
+    get_pos(v, box);
+
+    if (box[0] == item.box[0] && box[1] == item.box[1] &&
+        box[2] == item.box[2] && box[3] == item.box[3])
+        return;
+
+    // remove from lists the proxy is leaving
+    for (int x = item.box[0]; x < item.box[2]; x++)
+    for (int y = item.box[1]; y < item.box[3]; y++) {
+        if (overlaps(x, y, box))
+            continue;
+        remove_proxy(grid[GRID_INDEX(x, y)], proxy);
+    }
+
+    // add to lists the proxy is entering
+    for (int x = box[0]; x < box[2]; x++)
+    for (int y = box[1]; y < box[3]; y++) {
+        if (overlaps(x, y, item.box))
+            continue;
+        grid[GRID_INDEX(x, y)].push_back(proxy);
+    }
+
+    item.box[0] = box[0];
+    item.box[1] = box[1];
+    item.box[2] = box[2];
+    item.box[3] = box[3];
+}
---
+++ Chowdren/base/broadphase/grid.h
@@ -0,0 +1,112 @@
+#ifndef CHOWDREN_GRID_H
+#define CHOWDREN_GRID_H
+
+#include <vector>
+#include <algorithm>
+
+struct GridItem
+{
+    void * data;
+    int box[4];
+    int last_query_id;
+};
+
+typedef std::vector<int> GridItemList;
+
+#define GRID_INDEX(x, y) ((x) + (y) * width)
+#define GRID_SIZE 256
+
+class UniformGrid
+{
+public:
+    int width, height;
+    std::vector<GridItem> store;
+    GridItemList free_list;
+    GridItemList * grid;
+    int query_id;
+
+    UniformGrid();
+    ~UniformGrid();
+    int add(void * data, int v[4]);
+    void move(int proxy, int v[4]);
+    void remove(int proxy);
+    void clear();
+
+    template <typename T>
+    bool query(int v[4], T & callback);
+
+    template <typename T>
+    bool query(int proxy, T & callback);
+
+    void get_pos(int in[4], int out[4]);
+};
+
+#undef max
+#undef min
+
+inline int clamp(int v, int minval, int maxval)
+{
+    return std::min(maxval, std::max(v, minval));
+}
+
+inline void UniformGrid::get_pos(int in[4], int out[4])
+{
+    out[0] = clamp(in[0] / GRID_SIZE, 0, width-1);
+    out[1] = clamp(in[1] / GRID_SIZE, 0, height-1);
+    out[2] = clamp(in[2] / GRID_SIZE + 1, 1, width);
+    out[3] = clamp(in[3] / GRID_SIZE + 1, 1, height);
+}
+
+template <typename T>
+inline bool UniformGrid::query(int proxy, T & callback)
+{
+    GridItem & item = store[proxy];
+    GridItemList::iterator it;
+
+    query_id++;
+
+    for (int x = item.box[0]; x < item.box[2]; x++)
+    for (int y = item.box[1]; y < item.box[3]; y++) {
+        GridItemList & list = grid[GRID_INDEX(x, y)];
+
+        for (it = list.begin(); it != list.end(); it++) {
+            int index = *it;
+            if (index == proxy)
+                continue;
+            GridItem & vv = store[index];
+            if (vv.last_query_id == query_id)
+                continue;
+            vv.last_query_id = query_id;
+            if (!callback.on_callback(vv.data))
+                return false;
+        }
+    }
+    return true;
+}
+
+template <typename T>
+inline bool UniformGrid::query(int v[4], T & callback)
+{
+    GridItemList::iterator it;
+    int vv[4];
+    get_pos(v, vv);
+
+    query_id++;
+
+    for (int x = vv[0]; x < vv[2]; x++)
+    for (int y = vv[1]; y < vv[3]; y++) {
+        GridItemList & list = grid[GRID_INDEX(x, y)];
+
+        for (it = list.begin(); it != list.end(); it++) {
+            GridItem & vv = store[*it];
+            if (vv.last_query_id == query_id)
+                continue;
+            vv.last_query_id = query_id;
+            if (!callback.on_callback(vv.data))
+                return false;
+        }
+    }
+    return true;
+}
+
+#endif // CHOWDREN_GRID_H

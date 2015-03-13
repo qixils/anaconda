26a4da8e44910f2b79e6bfc341fb34837f4ff4b1
Mathias Kaerlev <matpow2@gmail.com>
2015-03-13 13:18:55 +0100

    Fix if check

--- Chowdren/base/run.sh
+++ Chowdren/base/run.sh
@@ -3,7 +3,7 @@
 GAMEDIR=`dirname "$(readlink -f "$0")"`
 cd "$GAMEDIR"
 
-if $(uname -m | grep '64'); then
+if [ $(uname -m | grep '64') ]; then
     "./bin64/Chowdren"
 else
     "./bin32/Chowdren"

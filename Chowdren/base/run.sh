b726334808ed7333c195dae4027a68cfcf66b373
Mathias Kaerlev <matpow2@gmail.com>
2015-03-13 13:08:51 +0100

    try and fix run.sh

--- Chowdren/base/run.sh
+++ Chowdren/base/run.sh
@@ -3,10 +3,8 @@
 GAMEDIR=`dirname "$(readlink -f "$0")"`
 cd "$GAMEDIR"
 
-MACHINE_TYPE=`uname -m`
-if [ ${MACHINE_TYPE} == 'x86_64' ]; then
-    ./bin64/Chowdren
+if $(uname -m | grep '64'); then
+    "./bin64/Chowdren"
 else
-    ./bin32/Chowdren
-  # 32-bit stuff here
+    "./bin32/Chowdren"
 fi
